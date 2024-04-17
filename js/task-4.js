2aa23413326643e9b8a5e01445537cb8a3346ad8
RomanHlushko <romaglusko346@gmail.com>
2024-04-17 20:20:54 +0300

    finish hw 7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+const handlerDataSubmit = (event) => {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    form.elements.password.value = "";
+    form.elements.email.value = "";
+    return alert("All form fields must be filled in");
+  }
+
+  const userData = {
+    email: email.trim(),
+    password: password.trim(),
+  };
+
+  console.log(userData);
+  form.reset();
+};
+
+loginForm.addEventListener("submit", handlerDataSubmit);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,50 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNum = document.querySelector("#controls input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const parentDivElem = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  if (amount === "") {
+    parentDivElem.innerHTML = "";
+    return;
+  } else if (amount < 1 || amount > 100) {
+    return;
+  }
+
+  parentDivElem.innerHTML = "";
+
+  for (let i = 0; i < amount; i++) {
+    const newDivElem = document.createElement("div");
+    parentDivElem.append(newDivElem);
+
+    const listOfNewDivElements = parentDivElem.querySelectorAll("div");
+
+    listOfNewDivElements.forEach((childDivElement, i) => {
+      let num = 0;
+
+      if (i === listOfNewDivElements.length - 1) {
+        num = (listOfNewDivElements.length - 1) * 10 + 30;
+
+        childDivElement.style.width = `${num}px`;
+        childDivElement.style.height = `${num}px`;
+        childDivElement.style.backgroundColor = getRandomHexColor();
+      }
+    });
+  }
+
+  inputNum.value = "";
+}
+
+const destroyElem = (event) => {
+  parentDivElem.innerHTML = "";
+  inputNum.value = "";
+};
+
+createBtn.addEventListener("click", (event) => {
+  createBoxes(inputNum.value);
+});
+destroyBtn.addEventListener("click", destroyElem);
